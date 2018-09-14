6f12288f9ad8c4cebe091cc9d5e4fa647dd8e59e
pinchukovartur <pinchukovartur@gmail.com>
2018-09-14 16:48:50 +0300

    narabotki udalenia

--- abtesting/consts.py
+++ abtesting/consts.py
@@ -1,3 +1,3 @@
 local_server = "http://192.168.88.29:1995/"
-dev_server = local_server#"https://treasures-development.appspot.com/"
-prod_server = local_server#"https://treasures-production.appspot.com/"
+dev_server = "https://treasures-development.appspot.com/"
+prod_server = "https://treasures-production.appspot.com/"
